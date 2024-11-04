81e44f30c1399a70510eeea743861c7247d099ee
Joshua Heim <j.heim@kentix.com>
2024-11-04 08:54:24 +0100

    combine projects

--- NoGo.js
+++ NoGo.js
@@ -1,10 +1,11 @@
-class NoGo {
-    constructor(person, place) {
+export default class NoGo {
+    constructor(person=null, place=null, day=null) {
         this.person = person;
         this.place = place;
+        this.day = day;
     }
 
-    check(randPer, randPl) {
-        return randPer === this.person && randPl === this.place;
+    check(randPer, randPl, day) {
+        return (this.person === null || randPer === this.person) && (this.place === null || randPl === this.place) && (this.day === null || day === this.day);
     }
 }
--- index.js
+++ index.js
@@ -1,17 +1,68 @@
-import NoGo from NoGo.js;
+import NoGo from "./NoGo.js";
 
-const nogos = [
-    new NoGo(1,2),
-    new NoGo(2,3),
+const days = [
+    'montag',
+    'dienstag',
+    'mittwoch',
+    'donnerstag',
+    'freitag'
 ];
 
-function generateEntry() {
-    const p = 0; //randomize
-    const pl = 0; //randomize
+const options = [
+    'BK',
+    'Stützel',
+    'Globus',
+    'Meaty Munch (Ranzi)',
+    'Bäcker',
+    'Globus-Pizza',
+];
 
-    if (nogos.any((nogo) => nogo.check(p,pl))) {
-        return generateEntry();
+const drivers = [
+    'Joshua',
+    'Kai',
+    'Carsten',
+    'Adriano',
+    'Dorian'
+];
+
+// Kommt noch
+const specials = [
+    'Mc Donalds',
+    'Döner',
+];
+
+const nogos = [
+    new NoGo(0,1),
+    new NoGo(4,null,1), // Dori Berufsschule
+    new NoGo(4,null,2), // Dori Berufsschule
+    new NoGo(2,null,4), // Carsten hat was dabei
+];
+
+function generate() {
+    let d = JSON.parse(JSON.stringify(drivers));
+    let op = JSON.parse(JSON.stringify(options));
+    days.forEach(day => {
+        let value = generateEntry(day, d, op);
+        document.getElementById('driver_'+day).innerHTML = d[value[0]];
+        document.getElementById('value_'+day).innerHTML = op[value[1]];
+        d = d.filter((item, index) => index !== value[0]);
+        op = op.filter((item, index) => index !== value[1]);
+    });
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateEntry(day, d, op) {
+    const p = randomInteger(0, d.length-1);
+    const pl = randomInteger(0, op.length-1);
+
+    if (nogos.some((nogo) => nogo.check(p,pl,day))) {
+        return generateEntry(day, d, op);
     } else {
         return [p,pl];
     }
 }
+
+document.getElementById('generator').addEventListener('click', generate);
